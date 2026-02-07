acfa5d725c8e99f0da876083da3ab70128033c09
Tanya <tanya210300@gmail.com>
2026-02-07 16:39:58 +0200

    fix

--- app/main.py
+++ app/main.py
@@ -21,18 +21,19 @@ class CarWashStation:
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
         for car in cars:
-            self.wash_single_car(car)
             income += self.calculate_washing_price(car)
+            self.wash_single_car(car)
         return round(income, 1)
 
+
     def calculate_washing_price(self, car: Car) -> float:
-        res = (
+        price = (
             car.comfort_class
             * (self.clean_power - car.clean_mark)
             * self.average_rating
-            / self.distance_from_city_center
         )
-        return round(res, 1)
+        return round(price, 1)
+
 
     def wash_single_car(
         self,
